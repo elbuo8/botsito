968b616ad5e53c04d2287652f52ece3616a6a00f
Yamil Asusta <hello@yamilasusta.com>
2017-01-24 20:19:56 -0400

    Add healthcheck

---
+++ scripts/healthcheck.js
@@ -0,0 +1,17 @@
+// Description:
+//   Respond with Hubot uptime
+//
+// Dependencies:
+//   "ms": "0.7.2"
+//
+// Author:
+//   @elbuo8
+
+const ms = require('ms');
+const startTime = Date.now();
+
+module.exports = function healthcheck(robot) {
+  robot.router.get('/health', (req, res) => {
+    return res.json({ uptime: ms(Date.now() - startTime) });
+  });
+}
